1807954c536e8b5fe01654dce40d82ca98c08fb2
Miika Leppänen <maleppanen@gmail.com>
2012-09-01 19:29:50 +0300

    Finish generator script

--- bin/generator.sh
+++ bin/generator.sh
@@ -11,15 +11,14 @@ _currbranch="$(grep "^*" < <("$_git" branch) | cut -d' ' -f2)"
 
 
 if [[ $_currbranch == $_sourcebranch ]]; then
-    echo "$_currbranch"
-    echo "$_sourcebranch"
-    # _site="$(mktemp -d /tmp/_site.XXXXXXXXX)"
-    # cd "$("$_git" rev-parse --show-toplevel)"
-    # "$_bundle" ${_opts[@]} . "$_site"
-    # "$_git" checkout "$_destbranch"
-
-    # cp -rf "$_site"/* .
-    
-    # rm -rfv "$_site"
+    _site="$(mktemp -d /tmp/_site.XXXXXXXXX)"
+    cd "$("$_git" rev-parse --show-toplevel)"
+    "$_bundle" ${_opts[@]} . "$_site"
+    "$_git" checkout "$_destbranch"
+    cp -rf "$_site"/* .
+    "$_git" add .
+    "$_git" commit -a -m "updated site @ $(date +"%F %T")"
+    rm -rfv "$_site"
+    "$_git" checkout "$_sourcebranch"
 fi
 
